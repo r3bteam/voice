f109ecc3c7fa23f7a6413ded70994d238e273bb8
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-04-06 19:46:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const moha1 = new Discord.Client();
+const moha2 = new Discord.Client();
+const moha3 = new Discord.Client();
+const moha4 = new Discord.Client();
+const moha5 = new Discord.Client();
+const moha6 = new Discord.Client();
+const moha7 = new Discord.Client();
+const moha8 = new Discord.Client();
+const moha9 = new Discord.Client();
+
+
+
+moha1.on('ready', () => {
+  var x = moha1.channels.get("564124111148679168");
+  if (x) x.join();
+  });
+
+
+  moha2.on('ready', () => {
+    var x = moha2.channels.get("564124111148679168");
+    if (x) x.join();
+    });
+
+    moha3.on('ready', () => {
+      var x = moha3.channels.get("564124111148679168");
+      if (x) x.join();
+      });
+
+      moha4.on('ready', () => {
+        var x = moha4.channels.get("564124111148679168");
+        if (x) x.join();
+        });
+
+        moha5.on('ready', () => {
+          var x = moha5.channels.get("564124111148679168");
+          if (x) x.join();
+          });
+
+          moha6.on('ready', () => {
+            var x = moha6.channels.get("564124111148679168");
+            if (x) x.join();
+            });
+
+            moha7.on('ready', () => {
+              var x = moha7.channels.get("564124111148679168");
+              if (x) x.join();
+              });
+
+              moha8.on('ready', () => {
+                var x = moha8.channels.get("564124111148679168");
+                if (x) x.join();
+                });
+
+                moha9.on('ready', () => {
+                  var x = moha9.channels.get("564124111148679168");
+                  if (x) x.join();
+                  });
+
+
+moha1.login(process.env.BOT_TOKEN1);
+moha2.login(process.env.BOT_TOKEN2);
+moha3.login(process.env.BOT_TOKEN3);
+moha4.login(process.env.BOT_TOKEN4);
+moha5.login(process.env.BOT_TOKEN5);
+moha6.login(process.env.BOT_TOKEN6);
+moha7.login(process.env.BOT_TOKEN7);
+moha8.login(process.env.BOT_TOKEN8);
+moha9.login(process.env.BOT_TOKEN9);
+
